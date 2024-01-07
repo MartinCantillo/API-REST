e979cd447c6bc066b6bcf8cd2668f2998e73e2ab
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-01-07 09:18:35 -0500

    Model persona and project configs

---
+++ rest/persona/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre= models.CharField(max_length=255)
+    apellido= models.CharField(max_length =255)
+    email= models.CharField(max_length=255)
+    def __str__(self) -> str:
+     return f'Persona: Id={self.id}, Nombre={self.nombre}, Apellido={self.apellido}, Email={self.email}'
--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'persona',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +125,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#cors configuration se colocan las urls de la api
+#CORS_ALLOWED_ORIGINS = [
+   # "https://example.com",
+    #"https://sub.example.com",
+    #"http://localhost:8080",
+    #"http://127.0.0.1:9000",
+#]
+
+CORS_ALLOWED_ORIGINS = [
+
+]
