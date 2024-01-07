f9f403d36432b037705d2731ca753811f4d7194b
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-01-07 12:03:59 -0500

    persona detail api

--- rest/persona/api.py
+++ rest/persona/api.py
@@ -1,10 +1,14 @@
+#este es para hacerlo por medio de clase
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Persona
 from .serializer import PersonaSerializer
+#Este es para hacerlo mediante funciones
+from rest_framework.decorators import api_view
 
 #Esto es cuando la api es basada en clase (APIView)
-#Creo la clase 
+#Basada en clase 
+'''
 class PersonaAPIView(APIView):
     #Creo los metodos
 
@@ -16,3 +20,31 @@ class PersonaAPIView(APIView):
         personas_serializers =PersonaSerializer(personas, many=True)
         #retorno response , en personas_serializers.data es donde se encuentra la info en .data 
         return Response(personas_serializers.data)
+'''
+
+#Basada en funciones
+@api_view(['GET','POST'])
+def Persona_api_view(request):
+    if request.method =='GET':
+        personas = Persona.objects.all()
+        personas_serializers=PersonaSerializer(personas, many=True)
+        return  Response(personas_serializers.data)
+    elif request.method=='POST':
+        #se descerializan los datos que vienen del request
+        persona_serializer=PersonaSerializer(data=request.data)
+        #valido si el serializer es valido
+        if persona_serializer.is_valid():
+            #entonces guardo la persona
+            persona_serializer.save()
+            #la retorno
+            return Response(persona_serializer.data)
+        #sino arrojo el error
+        return Response(persona_serializer.errors)
+    
+
+@api_view(['GET'])
+def Persona_detail_api_view(request, pk=None): #pk=none es para que sea opcional que coloquen el pk en la llamada
+    if request.method =='GET':
+        persona=Persona.objects.filter(id=pk).first() 
+        persona_serializer =PersonaSerializer(persona)   
+        return Response(persona_serializer.data)
--- rest/persona/urls.py
+++ rest/persona/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 #importo la clase de la api
-from .api import PersonaAPIView
+#from .api import PersonaAPIView
+#cuando es por funcion 
+from .api import Persona_api_view,Persona_detail_api_view
 
 #Creo las rutas
 
+#cuando es mediante clase 
+'''
 urlpatterns=[
   path('persona/', PersonaAPIView.as_view(), name='personaApi')
+ ]
+ '''
+
+#Cuando es mediante funcion
+urlpatterns=[
+  path('persona/', Persona_api_view, name='personaApi'),
+  path('persona_d/<int:pk>/',Persona_detail_api_view,name='persona_d')#<int:pk> para pasarle datos
  ]
